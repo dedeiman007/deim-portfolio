93a68a9b6c044e5a8237b7fe6787d744682f36cd
dede iman <deim@dedes-MacBook-Pro.local>
2026-03-04 18:06:49 +0800

    fix

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,4 @@
+
+import { viteNodeFetch } from "file:///Users/deim/Documents/project-viet/deim-portfolio/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
+export default () => viteNodeFetch.getManifest()
+    
